03c6fc93b8b7b6f3cdf8dfc5d90e395fd1dba4c8
Chas Nelson <github@chasnelson.co.uk>
2025-01-23 12:06:54 +0000

    feat: start warp engines with a working plugin

--- ckanext/file_uploader_js/tests/test_helpers.py
+++
@@ -1,7 +0,0 @@
-"""Tests for helpers.py."""
-
-import ckanext.file_uploader_js.helpers as helpers
-
-
-def test_file_uploader_js_hello():
-    assert helpers.file_uploader_js_hello() == "Hello, file_uploader_js!"
---
+++ react/components/FileInputComponent/index.js
@@ -0,0 +1,37 @@
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+import App from './src/App';
+
+const componentElement =
+  document.getElementById('FileInputComponent');
+const getAttr = key => {
+  const val = componentElement.getAttribute(`data-${key}`);
+  return ['None', ''].includes(val) ? null : val;
+};
+const
+  loadingHtml = componentElement.innerHTML,
+  maxResourceSize = parseInt(getAttr('maxResourceSize')),
+  lfsServer = getAttr('lfsServer'),
+  orgId = getAttr('orgId'),
+  datasetName = getAttr('datasetName');
+
+const existingResourceData = {
+  urlType: getAttr('existingUrlType'),
+  url: getAttr('existingUrl'),
+  sha256: getAttr('existingSha256'),
+  fileName: getAttr('existingFileName'),
+  size: getAttr('existingSize'),
+}
+
+const root = createRoot(componentElement);
+
+// wait for ckan.i18n to load
+window.addEventListener('load', function () {
+  root.render(
+    <App {...{
+      loadingHtml,
+      maxResourceSize, lfsServer, orgId,
+      datasetName, existingResourceData
+    }} />
+  );
+})
